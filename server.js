214eaab2815935022f827004cd14e05b0ef39ab7
shenshen <xiaoguaishou404@qq.com>
2023-06-26 05:19:29 +0800

    socket.io

---
+++ server.js
@@ -0,0 +1,26 @@
+// socker.io服务器
+const app = require('express')();
+const server = require('http').createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
